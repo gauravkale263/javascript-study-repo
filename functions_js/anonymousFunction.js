8ffb1b07734cbf662984b647e2f0c603057fcb59
Gaurav Kale <gauravkale2509@gmail.com>
2025-09-13 16:59:01 +0530

    just-practice

--- functions_js/anonymousFunction.js
+++ functions_js/anonymousFunction.js
@@ -3,3 +3,25 @@ let ans=function(){
     return 100;
 }
 console.log(ans());
+
+//change the vowel with _
+
+let makeChangeVowel=function(str){
+    let vowel="aeiouAEIOU"
+    let result="";
+    for(let char of str){
+        if(vowel.includes(char)){
+            result+='_'
+        }
+        else{
+            result+=char
+        }
+    }
+    return result
+   
+}
+let str="GAURAV KALE"
+console.log(makeChangeVowel(str))
+
+
+
