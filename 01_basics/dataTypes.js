b2943f4b4b14b8fe8119cc449e5bf75b5a73f625
Gaurav Kale <gauravkale2509@gmail.com>
2025-09-13 11:34:07 +0530

    dataTypes:learn-about-dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,17 @@
+let a=10;
+let b='Gaurav Kale';
+let c=true;
+let data=[
+    {name:'Gaurav', age:24},
+    {name:'Kale', age:26},
+    {name:'John', age:30}
+]
+console.table(data);
+
+/*
+   Primitive types->
+                    Number,String,Boolean,Null,Undefined,Symbol,BigInt
+   Reference(Non Primitive)->
+                    Array,Object,Function
+
+*/
