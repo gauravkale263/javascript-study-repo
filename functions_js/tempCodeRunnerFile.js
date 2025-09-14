5707b0163f8427693ceaa1185d537c40c2936b1d
Gaurav Kale <gauravkale2509@gmail.com>
2025-09-14 22:38:03 +0530

    learned-about-string

---
+++ functions_js/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+
+// //This is a simple callback function using promise to avoid callback hell
+// function mainFunction(anotherFun){
+//   return new Promise((resolve)=>{
+//     setTimeout(anotherFun,2000);
+//     resolve();
+//   })
+// }
+
+// function data(){
+//     console.log("This is required work");
+    
+// }
+
+// mainFunction(data).then(()=>{
+//     console.log("After promise");
+// });
